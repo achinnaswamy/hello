47b3a8af666ca7b7dc5755d858f60f4d95df8608
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 17:23:34 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import csv
 import requests
 with open ('out/tables/destination.csv', mode='wt', encoding='utf-8') as out_file:
      writer = csv.writer(out_file)
-     r = requests.get(r'https://www.reddit.com/r/tableau/.json')
+     r = requests.get('https://www.reddit.com/r/tableau/.json', headers = {'User-agent': 'Chrome'})
      r.text
      data = r.json()
      for child in data['data']['children']:
-          writer.writerow({child['kind'], child['kind']})
+          writer.writerow({child['data']['id'], child['data']['author']})
