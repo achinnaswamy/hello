ce9a9372cd7ddf73567094c752458e7dd22e3341
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 16:50:33 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ url = 'https://www.reddit.com/r/tableau/.json'
 json_obj = urllib2.urlopen(url)
 data = json.load(json_obj)
 for child in data['data']['children']:
-writer.writerow({child['data']['id'], child['data']['author']}) 
+    writer.writerow({child['data']['id'], child['data']['author']}) 
