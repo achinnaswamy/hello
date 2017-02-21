e173519d99cd27238e9f1db1a4eafdf53d85ac51
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 16:46:57 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import urllib2
 import json
 import csv
-csvdel = ‘,’
-csvquo = ‘”’
-with open (‘out/tables/destination.csv’, mode=’wt’, encoding=’utf-8’) as out_file:
-writer = csv.DictWriter(out_file, fieldnames=[‘col1’, ‘col2’], delimiter=csvdel, quotechar=csvquo)
-writer.writeheader()
-url = ‘https://www.reddit.com/r/tableau/.json’
+f =  open ('out/tables/destination2.csv')
+writer = csv.writer(f)
+url = 'https://www.reddit.com/r/tableau/.json'
 json_obj = urllib2.urlopen(url)
 data = json.load(json_obj)
-for child in data[‘data’][‘children’]:
-writer.writerow({‘col1’: child[‘data’][‘id’], ‘col2’: child[‘data’][‘author’]})
+for child in data['data']['children']:
+writer.writerow({child['data']['id'], child['data']['author']}) 
