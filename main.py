9d59a526a3a3c149254aee78a16c74d991a4b3f8
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 16:59:09 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import csv
 with open ('out/tables/destination.csv', mode='wt', encoding='utf-8') as out_file:
-writer = csv.writer(out_file)
+     writer = csv.writer(out_file)
 url = 'https://www.reddit.com/r/tableau/.json'
 json_obj = urllib2.urlopen(url)
 data = json.load(json_obj)
