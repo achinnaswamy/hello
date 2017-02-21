23b17c528d3f2c991715db1c911742d958ff5e72
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 17:02:13 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 import csv
+import urllib2
 with open ('out/tables/destination.csv', mode='wt', encoding='utf-8') as out_file:
      writer = csv.writer(out_file)
 url = 'https://www.reddit.com/r/tableau/.json'
 json_obj = urllib2.urlopen(url)
 data = json.load(json_obj)
 for child in data['data']['children']:
-    writer.writerow({child['data']['id'], child['data']['author']}) 
+     writer.writerow({child['data']['id'], child['data']['author']})
