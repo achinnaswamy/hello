145774f2ea4cc8a407a5c3fc260e6fbf92bcd195
achinnaswamy <archana.chinnaswamy@interworks.com>
2017-02-21 17:10:09 -0600

    commit
    commit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import csv
 import requests
 with open ('out/tables/destination.csv', mode='wt', encoding='utf-8') as out_file:
      writer = csv.writer(out_file)
-r = requests.get(r'https://www.reddit.com/r/tableau/.json')
-r.text
-data = r.json()
-for child in data['data']['children']:
-     writer.writerow({child['data']['id'], child['data']['author']})
+     r = requests.get(r'https://www.reddit.com/r/tableau/.json')
+     r.text
+     data = r.json()
+     for child in data['data']['children']:
+          writer.writerow({child['data']['id'], child['data']['author']})
